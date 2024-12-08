0426f4e2498f60cf4fc4ec80684fed6d3fbc9919
Muhammad Bilal <muhammadbilaal098@gmail.com>
2024-12-08 14:05:31 +0500

    add even id waypoints

--- BackEnd/utils/busRoutes.js
+++ BackEnd/utils/busRoutes.js
@@ -49,30 +49,81 @@ waypoints: [
         latitude: 24.8554,
         longitude: 67.2473,
       },
-
-        waypoints: [
-      { latitude: 24.9069, longitude: 67.1386, sequence: 1 },
-      { latitude: 24.9100, longitude: 67.1400, sequence: 2 },
-      { latitude: 24.9130, longitude: 67.1450, sequence: 3 },
-      { latitude: 24.9150, longitude: 67.1500, sequence: 4 },
-      { latitude: 24.9170, longitude: 67.1550, sequence: 5 },
-      { latitude: 24.9200, longitude: 67.1600, sequence: 6 },
-      { latitude: 24.9220, longitude: 67.1650, sequence: 7 },
-      { latitude: 24.9240, longitude: 67.1700, sequence: 8 },
-      { latitude: 24.9260, longitude: 67.1750, sequence: 9 },
-      { latitude: 24.9280, longitude: 67.1800, sequence: 10 },
-      { latitude: 24.9300, longitude: 67.1850, sequence: 11 },
-      { latitude: 24.9320, longitude: 67.1900, sequence: 12 },
-      { latitude: 24.9340, longitude: 67.1950, sequence: 13 },
-      { latitude: 24.9360, longitude: 67.2000, sequence: 14 },
-      { latitude: 24.9380, longitude: 67.2050, sequence: 15 },
-      { latitude: 24.9400, longitude: 67.2100, sequence: 16 },
-      { latitude: 24.9420, longitude: 67.2150, sequence: 17 },
-      { latitude: 24.9440, longitude: 67.2200, sequence: 18 },
-      { latitude: 24.9460, longitude: 67.2250, sequence: 19 },
-      { latitude: 24.833093, longitude: 66.971648, sequence: 20 },
-
-            ]
+      waypoints: [
+          { 
+            "sequence": 1, 
+            "name": "Nagan Chowrangi", 
+            "latitude": 24.9645, 
+            "longitude": 67.0671 
+          },
+          { 
+            "sequence": 2, 
+            "name": "Shafiq Morr", 
+            "latitude": 24.9609, 
+            "longitude": 67.0514 
+          },
+          { 
+            "sequence": 3, 
+            "name": "Sohrab Goth", 
+            "latitude": 24.9043, 
+            "longitude": 67.0803 
+          },
+          { 
+            "sequence": 4, 
+            "name": "Gulshan Chowrangi", 
+            "latitude": 24.9378, 
+            "longitude": 67.0737 
+          },
+          { 
+            "sequence": 5, 
+            "name": "NIPA", 
+            "latitude": 24.9307, 
+            "longitude": 67.0865 
+          },
+          { 
+            "sequence": 6, 
+            "name": "Johar Morr", 
+            "latitude": 24.9184, 
+            "longitude": 67.0807 
+          },
+          { 
+            "sequence": 7, 
+            "name": "COD", 
+            "latitude": 24.8697, 
+            "longitude": 67.0291 
+          },
+          { 
+            "sequence": 8, 
+            "name": "Drigh Road Station", 
+            "latitude": 24.9130, 
+            "longitude": 67.0628 
+          },
+          { 
+            "sequence": 9, 
+            "name": "Colony Gate", 
+            "latitude": 24.8795, 
+            "longitude": 67.0526 
+          },
+          { 
+            "sequence": 10, 
+            "name": "Shah Faisal Colony", 
+            "latitude": 24.8729, 
+            "longitude": 67.0950 
+          },
+          { 
+            "sequence": 11, 
+            "name": "Singer Chowrangi", 
+            "latitude": 24.9309, 
+            "longitude": 67.0937 
+          },
+          { 
+            "sequence": 12, 
+            "name": "Landhi Road", 
+            "latitude": 24.8604, 
+            "longitude": 67.1330 
+          }
+        ]
+      ,
     },
     {
       id: 3,
@@ -87,6 +138,8 @@ waypoints: [
         latitude: 24.9223,
         longitude: 67.0925,
       },
+
+    
     },
     {
       id: 4,
@@ -101,6 +154,28 @@ waypoints: [
         latitude: 24.85,
         longitude: 67.0113,
       },
+
+      waypoints: [
+        { "sequence": 1, "name": "Power House", "latitude": 24.8924, "longitude": 67.0562 },
+        { "sequence": 2, "name": "UP Mor", "latitude": 24.9382, "longitude": 67.1095 },
+        { "sequence": 3, "name": "Nagan Chowrangi", "latitude": 24.9645, "longitude": 67.0671 },
+        { "sequence": 4, "name": "Shafiq Morr", "latitude": 24.9609, "longitude": 67.0514 },
+        { "sequence": 5, "name": "Sohrab Goth", "latitude": 24.9043, "longitude": 67.0803 },
+        { "sequence": 6, "name": "Shahrah e Pakistan", "latitude": 24.9172, "longitude": 67.0806 },
+        { "sequence": 7, "name": "Ayesha Manzil", "latitude": 24.9431, "longitude": 67.0534 },
+        { "sequence": 8, "name": "Liaquatabad 10", "latitude": 24.9335, "longitude": 67.0469 },
+        { "sequence": 9, "name": "Laloo Khait", "latitude": 24.9385, "longitude": 67.0502 },
+        { "sequence": 10, "name": "Teen Hati", "latitude": 24.9387, "longitude": 67.0451 },
+        { "sequence": 11, "name": "Jehangir Road", "latitude": 24.8601, "longitude": 67.0245 },
+        { "sequence": 12, "name": "Numaish", "latitude": 24.8619, "longitude": 67.0228 },
+        { "sequence": 13, "name": "Mobile Market", "latitude": 24.8616, "longitude": 67.0229 },
+        { "sequence": 14, "name": "Urdu Bazar", "latitude": 24.8605, "longitude": 67.0223 },
+        { "sequence": 15, "name": "Civil Hospital", "latitude": 24.8574, "longitude": 67.0164 },
+        { "sequence": 16, "name": "City Court", "latitude": 24.8602, "longitude": 67.0193 },
+        { "sequence": 17, "name": "Light House", "latitude": 24.8604, "longitude": 67.0175 },
+        { "sequence": 18, "name": "Bolton Market", "latitude": 24.8610, "longitude": 67.0195 },
+        { "sequence": 19, "name": "Tower", "latitude": 24.8609, "longitude": 67.0119 }
+      ]
     },
     {
       id: 9,
@@ -115,6 +190,7 @@ waypoints: [
         latitude: 24.85,
         longitude: 67.0113,
       },
+      
     },
     {
       id: 10,
@@ -129,6 +205,37 @@ waypoints: [
         latitude: 24.8012,
         longitude: 67.1745,
       },
+
+      waypoints: [
+        { "sequence": 1, "name": "Numaish", "latitude": 24.8619, "longitude": 67.0228 },
+        { "sequence": 2, "name": "Taj Complex", "latitude": 24.8604, "longitude": 67.0293 },
+        { "sequence": 3, "name": "Capri Cinema", "latitude": 24.8601, "longitude": 67.0276 },
+        { "sequence": 4, "name": "Mobile Market", "latitude": 24.8616, "longitude": 67.0229 },
+        { "sequence": 5, "name": "Regal", "latitude": 24.8612, "longitude": 67.0315 },
+        { "sequence": 6, "name": "Abdullah Haroon", "latitude": 24.8608, "longitude": 67.0299 },
+        { "sequence": 7, "name": "Zainab Market", "latitude": 24.8593, "longitude": 67.0269 },
+        { "sequence": 8, "name": "Fawara Chowk", "latitude": 24.8590, "longitude": 67.0271 },
+        { "sequence": 9, "name": "Metropole", "latitude": 24.8559, "longitude": 67.0292 },
+        { "sequence": 10, "name": "Frere Hall", "latitude": 24.8603, "longitude": 67.0304 },
+        { "sequence": 11, "name": "PSO Head Office", "latitude": 24.8529, "longitude": 67.0431 },
+        { "sequence": 12, "name": "3 Talwar", "latitude": 24.8069, "longitude": 67.0562 },
+        { "sequence": 13, "name": "Ocean Mall", "latitude": 24.8181, "longitude": 67.0492 },
+        { "sequence": 14, "name": "2 Talwar", "latitude": 24.8090, "longitude": 67.0566 },
+        { "sequence": 15, "name": "Abdullah Shah Ghazi Mazar", "latitude": 24.8196, "longitude": 67.0437 },
+        { "sequence": 16, "name": "Bagh Ibn E Qasim", "latitude": 24.8057, "longitude": 67.0473 },
+        { "sequence": 17, "name": "Dolmen Mall Clifton", "latitude": 24.8062, "longitude": 67.0385 },
+        { "sequence": 18, "name": "Seaview Clock Tower", "latitude": 24.8056, "longitude": 67.0390 },
+        { "sequence": 19, "name": "Khayaban e Ittehad", "latitude": 24.8032, "longitude": 67.0625 },
+        { "sequence": 20, "name": "DHA Phase 7", "latitude": 24.8215, "longitude": 67.0711 },
+        { "sequence": 21, "name": "DHA Phase 6", "latitude": 24.8309, "longitude": 67.0672 },
+        { "sequence": 22, "name": "DHA Phase 2", "latitude": 24.8554, "longitude": 67.0430 },
+        { "sequence": 23, "name": "KPT Interchange", "latitude": 24.8141, "longitude": 67.0426 },
+        { "sequence": 24, "name": "Korangi Crossing", "latitude": 24.8324, "longitude": 67.1582 },
+        { "sequence": 25, "name": "Indus Hospital", "latitude": 24.8610, "longitude": 67.1403 },
+        { "sequence": 26, "name": "CBM University", "latitude": 24.8833, "longitude": 67.1052 },
+        { "sequence": 27, "name": "PARCO", "latitude": 24.8281, "longitude": 67.0703 },
+        { "sequence": 28, "name": "Ibrahim Hyderi", "latitude": 24.8421, "longitude": 67.1712 }
+      ]
     },
     {
       id: 11,
@@ -157,6 +264,33 @@ waypoints: [
         latitude: 24.8551,
         longitude: 67.0342,
       },
+      waypoints: [
+    { "sequence": 1, "name": "Naddi Kinara", "latitude": 24.8729, "longitude": 67.1052 },
+    { "sequence": 2, "name": "Khokhrapar", "latitude": 24.8660, "longitude": 67.1233 },
+    { "sequence": 3, "name": "Saudabad Chowrangi", "latitude": 24.8645, "longitude": 67.1473 },
+    { "sequence": 4, "name": "RCD Ground", "latitude": 24.8282, "longitude": 67.1490 },
+    { "sequence": 5, "name": "Kalaboard", "latitude": 24.8650, "longitude": 67.1435 },
+    { "sequence": 6, "name": "Malir 15", "latitude": 24.8689, "longitude": 67.1514 },
+    { "sequence": 7, "name": "Malir Mandir", "latitude": 24.8584, "longitude": 67.1568 },
+    { "sequence": 8, "name": "Malir Session Court", "latitude": 24.8561, "longitude": 67.1582 },
+    { "sequence": 9, "name": "Murghi Khana", "latitude": 24.8493, "longitude": 67.1690 },
+    { "sequence": 10, "name": "Quaidabad", "latitude": 24.8277, "longitude": 67.1516 },
+    { "sequence": 11, "name": "Dawood Chowrangi", "latitude": 24.8362, "longitude": 67.1609 },
+    { "sequence": 12, "name": "Babar Market", "latitude": 24.8482, "longitude": 67.1619 },
+    { "sequence": 13, "name": "Landhi Road", "latitude": 24.8270, "longitude": 67.1750 },
+    { "sequence": 14, "name": "Nasir Jump", "latitude": 24.8420, "longitude": 67.1763 },
+    { "sequence": 15, "name": "Indus Hospital", "latitude": 24.8610, "longitude": 67.1403 },
+    { "sequence": 16, "name": "Korangi Crossing", "latitude": 24.8324, "longitude": 67.1582 },
+    { "sequence": 17, "name": "Qayyumabad", "latitude": 24.8289, "longitude": 67.1660 },
+    { "sequence": 18, "name": "Defence Mor", "latitude": 24.8069, "longitude": 67.0562 },
+    { "sequence": 19, "name": "National Medical Center", "latitude": 24.8328, "longitude": 67.0653 },
+    { "sequence": 20, "name": "Gora Qabristan", "latitude": 24.8060, "longitude": 67.0472 },
+    { "sequence": 21, "name": "FTC", "latitude": 24.8614, "longitude": 67.0322 },
+    { "sequence": 22, "name": "Jutt Land", "latitude": 24.8276, "longitude": 67.1432 },
+    { "sequence": 23, "name": "Lines Area", "latitude": 24.8612, "longitude": 67.0212 },
+    { "sequence": 24, "name": "Army Public School", "latitude": 24.8651, "longitude": 67.0241 },
+    { "sequence": 25, "name": "Lucky Star Saddar", "latitude": 24.8602, "longitude": 67.0217 }
+  ]
     },
   ];
   
